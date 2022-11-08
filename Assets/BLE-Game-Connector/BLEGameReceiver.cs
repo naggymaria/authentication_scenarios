dd57f153d220e8659bbca8cc40c2ae600381b6d9
Margarida Felício <43440004+naggymaria@users.noreply.github.com>
2022-11-08 12:59:54 +0000

    Add files via upload

---
+++ Assets/BLE-Game-Connector/BLEGameReceiver.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class BLEGameReceiver : MonoBehaviour
+{
+    private Thread _receiveThread;
+    private UdpClient _client;
+
+    public static int Port = 2000;//port must be defined in BLE (for sending)
+    public static bool IsConnected, CanConnect;
+	public static float bottom_left=0, bottom_right=0, top_left=0, top_right=0, center_gravity_X=0, center_gravity_y=0;
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        if (CanConnect && !IsConnected)
+        {
+            Init();
+            CanConnect = false;
+        }
+        else if (CanConnect && IsConnected)
+        {
+            CloseConnection();
+        }
+    }
+
+    private void Init()
+    {
+        _receiveThread = new Thread(ReceiveData) {IsBackground = true};
+        _receiveThread.Start();
+        IsConnected = true;
+    }
+
+
+    private void ReceiveData()
+    {
+        _client = new UdpClient(Port);
+
+        while (IsConnected)
+        {
+            try
+            {
+                var ip = new IPEndPoint(IPAddress.Any, Port);
+                var udpdata = _client.Receive(ref ip);
+                var data = Encoding.UTF8.GetString(udpdata);
+
+                TranslateData(data);
+            }
+
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+	//Create the variables of the objects that you want to update from the BLE and drag it in the inspector
+    //Example: public PlayerController PlayerController;
+    private void TranslateData(string data)
+    {
+
+        string[] separators = { "[$]", "[$$]", "[$$$]", ",", ";", " " };
+
+        var words = data.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            if(words[i] == "LocalIP")
+            {
+                GameBLESender._ip = words[i + 1];
+
+                Debug.Log(GameBLESender._ip);
+
+                if (GameBLESender._ip != null)
+                {
+                    GameBLESender.Instance.Init();
+                }                    
+            }
+
+            if (words[i] == "Height")
+            {
+                _2mStepTest_Manager.height = float.Parse(words[i + 1]);
+            }
+
+            if(words[i] == "LocalIP")
+            {
+                GameBLESender._ip = words[i + 1];
+            }
+
+			if (words [i].Equals ("Bottom_Left")) {
+				bottom_left = float.Parse(words [i + 2]); 
+			} else if (words [i].Equals ("Bottom_Right")) {
+				bottom_right = float.Parse(words [i + 2]); 
+			} else if (words [i].Equals ("Top_Left")) {
+				top_left = float.Parse(words [i + 2]); 
+			} else if (words [i].Equals ("Top_Right")) {
+				top_right =float.Parse(words [i + 2]); 
+			} else if (words [i].Equals ("CGravity_X")) {
+				center_gravity_X =float.Parse(words [i + 2]); 
+			} else if (words [i].Equals ("CGravity_Y")) {
+				center_gravity_y = float.Parse(words [i + 2]); 
+			}
+			else
+			{
+				;// do nothing 
+			}	
+        }
+    }
+
+    private void CloseConnection()
+    {
+        if (IsConnected)
+        {
+            _receiveThread.Abort();
+            if(_client != null)
+                _client.Close();
+            IsConnected = false;
+            CanConnect = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        CloseConnection();
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseConnection();
+    }
+}
+
---
+++ Assets/BLE-Game-Connector/GameBLESender.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class GameBLESender : MonoBehaviour
+{
+    public static GameBLESender Instance { get; set; }
+    public static string _ip;
+    private const int Port = 1204;
+    private IPEndPoint _remoteEndPoint;
+    private UdpClient _client;
+
+    public static bool IsGameSending;
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        //_ip = LocalIpAdress();
+        //Init();
+    }
+	        
+    public void Init()
+    {
+        Debug.Log("init");
+        if(_ip != null)
+        {
+            _remoteEndPoint = new IPEndPoint(IPAddress.Parse(_ip), Port);
+            _client = new UdpClient();
+        }
+
+
+        if(_remoteEndPoint != null)
+        {
+            SendString("Game,Connected to Game");
+            Debug.Log("Game,Connected to Game");
+        }
+            
+    }
+
+	//Invoke this function whenever you want to send a GameVariable update to the BLE
+	//Example: GameBLESender.Instance.SendString("GameVariable,BallSpeed,float," + speed + "," + "1,20");
+	public void SendString(string message)
+    {
+        try
+        {
+            if (message != "")
+            {
+                var data = Encoding.UTF8.GetBytes(message);
+                _client.Send(data, data.Length, _remoteEndPoint);
+            }
+        }
+
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+
+    private string LocalIpAdress()
+    {
+        if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+        {
+            return null;
+        }
+
+        var localIp = "";
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+
+        foreach (var ip in host.AddressList)
+        {
+            Debug.Log(ip.AddressFamily);
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIp = ip.ToString();
+            }
+        }
+        return localIp;
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (_client != null)
+            _client.Close();
+    }
+}
+
